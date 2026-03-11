f9d5eec9954db1fef24fc98ca140967d71ae9109
Shankar Dutta <mr.shankardutta@gmail.com>
2026-03-11 22:19:29 +0530

    Ui library init - shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,23 @@
 import { ReactNode } from "react";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 type RootLayoutProp = {
-  children: ReactNode;
+	children: ReactNode;
 };
 
 const RootLayout = ({ children }: Readonly<RootLayoutProp>) => {
-  return (
-    <html lang="en">
-      <body>
-        <main className="mx-auto max-w-7xl px-6 py-3">{children}</main>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={cn("font-sans", geist.variable)}>
+			<body>
+				<main className="mx-auto max-w-7xl px-6 py-3">{children}</main>
+			</body>
+		</html>
+	);
 };
 
 export default RootLayout;
